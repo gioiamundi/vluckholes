0f81875a21eeda0c08c3d308fb38ef9c8553c2b3
Glitch (aframe) <none>
2021-08-02 22:09:15 +0000

    🚜🐚 Checkpoint ./public/js/sphereexpand.js:32595434/406 ./index.html:32595434/632

--- public/js/sphereexpand.js
+++ public/js/sphereexpand.js
@@ -4,9 +4,11 @@
 
       let homeworldelements = document.querySelectorAll(".homeworld");
       let sky = document.querySelector("#sky");
+      let modelisla = document.querySelector("#islaflotante");
 
       let sphereloader = () => {
       sky.setAttribute("src", "#beach");
+      modelisla.setAttribute("src", "#islaflotante")
       homeworldelements.forEach((homeworldelement) => {
       homeworldelement.setAttribute("visible", false)})
       }
