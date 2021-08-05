c3c84e3c4023e012b54509b134e48e51e05944b7
Glitch (aframe) <none>
2021-08-05 12:07:28 +0000

    🏔🚎 Checkpoint ./public/js/backhome.js:32595434/173 ./index.html:32595434/216

--- public/js/backhome.js
+++ public/js/backhome.js
@@ -6,10 +6,12 @@
       let homeworldelements = document.querySelectorAll(".homeworld");
       let sky = document.querySelector("#sky");
       let videosphere = document.querySelector("#videosphereentity");
+      let model = document.querySelector("#tokyomodel");
 
       
       let gobackhome = () => {
       sky.setAttribute("src", "#infinitociudad");
+      model.setAttribute("src", "#tokyomodel");
       videosphere.setAttribute("radius", ".7");
       homeworldelements.forEach((homeworldelement) => {
       homeworldelement.setAttribute("visible", true)})
